9121101ac3e275929666b4fc7f28632f0d71bc4a
silencej <owen263@gmail.com>
2022-05-27 20:59:59 +0800

    Use latest OS and trac

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM ubuntu:xenial
-MAINTAINER = Di Xu <stephenhsu90@gmail.com>
+FROM ubuntu:22.04
+MAINTAINER = silencej <owen263@gmail.com>
 ENV TRAC_ADMIN_NAME trac_admin
 ENV TRAC_ADMIN_PASSWD passw0rd
 ENV TRAC_PROJECT_NAME trac_project
@@ -8,20 +8,16 @@ ENV TRAC_INI $TRAC_DIR/conf/trac.ini
 ENV DB_LINK sqlite:db/trac.db
 EXPOSE 8123
 
-RUN apt-get update && apt-get install -y trac python-babel \
-   libapache2-mod-wsgi python-pip && apt-get -y clean
-RUN pip install --upgrade Babel Trac
-RUN mkdir -p $TRAC_DIR
-RUN trac-admin $TRAC_DIR initenv $TRAC_PROJECT_NAME $DB_LINK
-RUN trac-admin $TRAC_DIR deploy /tmp/deploy
-RUN mv /tmp/deploy/* $TRAC_DIR
-RUN htpasswd -b -c $TRAC_DIR/.htpasswd $TRAC_ADMIN_NAME $TRAC_ADMIN_PASSWD
-RUN trac-admin $TRAC_DIR permission add $TRAC_ADMIN_NAME TRAC_ADMIN
-RUN chown -R www-data: $TRAC_DIR
-RUN chmod -R 775 $TRAC_DIR
-RUN echo "Listen 8123" >> /etc/apache2/ports.conf
-ADD trac.conf /etc/apache2/sites-available/trac.conf
-RUN sed -i 's|$AUTH_NAME|'"$TRAC_PROJECT_NAME"'|g' /etc/apache2/sites-available/trac.conf
-RUN sed -i 's|$TRAC_DIR|'"$TRAC_DIR"'|g' /etc/apache2/sites-available/trac.conf
-RUN a2dissite 000-default && a2ensite trac.conf
-CMD service apache2 stop && apache2ctl -D FOREGROUND
+# Run this in China
+RUN sed -i 's/archive/cn.archive/g' /etc/apt/sources.list
+
+# Install apache2-utils for `htdigest`
+RUN apt-get update && apt-get install -y python2 python-pip apache2-utils
+
+# Run this in China
+RUN pip2 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+RUN pip2 install --upgrade babel docutils pygments textile trac pytz TracAccountManager
+COPY docker-entrypoint.sh /
+RUN chmod a+x /docker-entrypoint.sh
+ENTRYPOINT /docker-entrypoint.sh
---
+++ docker-entrypoint.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = "tracd" ]; then
+    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+
+        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+            case "$f" in
+                *.sh)
+                    if [ -x "$f" ]; then
+                        echo >&3 "$0: Launching $f";
+                        "$f"
+                    else
+                        # warn on shell scripts without exec bit
+                        echo >&3 "$0: Ignoring $f, not executable";
+                    fi
+                    ;;
+                *) echo >&3 "$0: Ignoring $f";;
+            esac
+        done
+
+        echo >&3 "$0: Configuration complete; ready for start up"
+    else
+        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+    fi
+fi
+
+#----------
+
+if [[ -f "$TRAC_DIR/conf/trac.ini" ]]; then
+  echo "$TRAC_DIR already initialized, ignore initenv"
+else
+  mkdir -p $TRAC_DIR
+  trac-admin $TRAC_DIR initenv $TRAC_PROJECT_NAME $DB_LINK
+  # trac-admin $TRAC_DIR deploy /tmp/deploy
+  # mv /tmp/deploy/* $TRAC_DIR
+fi
+
+# htpasswd -b -c $TRAC_DIR/.htpasswd $TRAC_ADMIN_NAME $TRAC_ADMIN_PASSWD
+trac-admin $TRAC_DIR permission add $TRAC_ADMIN_NAME TRAC_ADMIN || echo "Ignore"
+# chown -R www-data: $TRAC_DIR
+# chmod -R 775 $TRAC_DIR
+
+# echo "Listen 8123" >> /etc/apache2/ports.conf
+# trac.conf /etc/apache2/sites-available/trac.conf
+# sed -i 's|$AUTH_NAME|'"$TRAC_PROJECT_NAME"'|g' /etc/apache2/sites-available/trac.conf
+# sed -i 's|$TRAC_DIR|'"$TRAC_DIR"'|g' /etc/apache2/sites-available/trac.conf
+# a2dissite 000-default && a2ensite trac.conf
+# service apache2 stop && apache2ctl -D FOREGROUND
+
+if [[ -f "$TRAC_DIR/users.htdigest" ]]; then
+  echo "$TRAC_DIR/users.htdigest exists, ignore generating it"
+else
+  digest="$( printf "%s:%s:%s" "$TRAC_ADMIN_NAME" "$TRAC_PROJECT_NAME" "$TRAC_ADMIN_PASSWD" | 
+           md5sum | awk '{print $1}' )"
+  printf "%s:%s:%s\n" "$TRAC_ADMIN_NAME" "$TRAC_PROJECT_NAME" "$digest" > "$TRAC_DIR/users.htdigest"
+fi
+
+# exec "$@"
+tracd --port 8123 -r --auth="*,$TRAC_DIR/users.htdigest,$TRAC_PROJECT_NAME" $TRAC_DIR
